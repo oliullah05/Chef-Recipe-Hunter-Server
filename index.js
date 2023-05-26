1f22fc1ead4bb98083e01bb293022e9c1d22ba8d
oliullah05 <mdolihasan@gmail.com>
2023-05-26 15:02:50 +0600

    Chif Hunter Server Site Data Add And Fetch Done

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// https://assignment-10-client-oliullah05.vercel.app/allchefdata
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+const chefData = require("./chefData.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/allchefdata", (req, res) => {
+  res.send(chefData);
+});
+
+app.get("/allchefdata/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefData.find((chef) => chef.id == id);
+  res.send(selectedChef);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
